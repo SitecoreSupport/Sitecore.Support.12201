a4a46e21c8eb3da5ebf37c43912ad621006ff9cb
AntonKechashin <atk@sitecore.net>
2018-03-28 14:22:01 +0300

    Initial commit

---
+++ src/Sitecore.Support.12201/XA/Feature/Navigation/Repositories/Navigation/NavigationRepository.cs
@@ -0,0 +1,38 @@
+﻿namespace Sitecore.Support.XA.Feature.Navigation.Repositories.Navigation
+{
+  using System.Linq;
+  using Sitecore.Data.Items;
+  using Sitecore.XA.Feature.Navigation;
+  using Sitecore.XA.Feature.Navigation.Repositories.Breadcrumb;
+  using Sitecore.XA.Foundation.RenderingVariants.Repositories;
+  using Sitecore.XA.Foundation.SitecoreExtensions.Utils;
+
+  public class NavigationRepository : Sitecore.XA.Feature.Navigation.Repositories.Navigation.NavigationRepository
+  {
+    public NavigationRepository(IVariantsRepository variantsRespository, IBreadcrumbRepository breadcrumbRepository) : base(variantsRespository, breadcrumbRepository)
+    {
+    }
+
+    protected override string BuildNavigationClass(Item item)
+    {
+      var lookup = ItemUtils.Lookup(item[Templates._Navigable.Fields.NavigationClass], Context.Database);
+      string navigationClass = lookup.Select(i => i["Value"]).FirstOrDefault();
+
+      var variantItem = VariantsRespository.VariantItem;
+      if (variantItem != null && !string.IsNullOrWhiteSpace(variantItem[Sitecore.XA.Foundation.RenderingVariants.Templates.VariantDefinition.Fields.ItemCssClassField]))
+      {
+        #region Original code
+        //string cssClass = item[variantItem[Sitecore.XA.Foundation.RenderingVariants.Templates.VariantDefinition.Fields.ItemCssClassField]]; 
+        #endregion
+        #region Changed code
+        string cssClass = variantItem[Sitecore.XA.Foundation.RenderingVariants.Templates.VariantDefinition.Fields.ItemCssClassField]; 
+        #endregion
+        if (!string.IsNullOrWhiteSpace(cssClass))
+        {
+          navigationClass = $"{navigationClass} {cssClass}".Trim();
+        }
+      }
+      return navigationClass;
+    }
+  }
+}
---
+++ src/Sitecore.Support.12201/XA/Foundation/IoC/ServiceCollection/RegisterSupportNavigationRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace Sitecore.Support.XA.Foundation.IoC.ServiceCollection
+{
+  using Microsoft.Extensions.DependencyInjection;
+  using Sitecore.DependencyInjection;
+  using Sitecore.XA.Feature.Navigation.Repositories.Navigation;
+
+  public class RegisterSupportNavigationRepository : IServicesConfigurator
+  {
+    public void Configure(IServiceCollection serviceCollection)
+    {
+      serviceCollection.AddSingleton<INavigationRepository, Sitecore.Support.XA.Feature.Navigation.Repositories.Navigation.NavigationRepository>();
+    }
+  }
+}
